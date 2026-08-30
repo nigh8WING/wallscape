a7bf27b637283162ddf03143dddcd8910f7c0732
Sheikh Hossain Bin Bakhtiar <skhossain799@gmail.com>
2026-08-30 19:38:13 +0600

    fix(windows): bundle runtime DLLs, GTK schemas, loaders and embed official logo into Windows installer

--- src/static_wallpaper.c
+++ src/static_wallpaper.c
@@ -9,7 +9,15 @@
 #include <gio/gio.h>
 #include <stdio.h>
 #include <string.h>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
+#else
 #include <strings.h>
+#endif
 
 static const char *const STATIC_EXTS[] = {
     ".jpg", ".jpeg", ".png", ".webp", ".bmp", ".svg", ".gif", NULL
